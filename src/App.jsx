9b0f935c9d9b571570309cfbb48657c9fb71aaf8
Dorra Ellouz <dorra021007@gmail.com>
2024-01-14 00:45:47 +0000

    message

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,82 @@
 import { useEffect, useState } from "react";
 import ShopComponent from "./ShopComponent";
-export default function  App(){
-    const [cookies, setCookies] =useState (0);
-    const [CookiesPerSecond, setCookiesPerSecond] =useState (1);
+export default function  App()
+{
+    const [cookies, setCookies] =useState (parseInt(localStorage.getItem("cookieCount"))||0);
+    const [CookiesPerSecond, setCookiesPerSecond] =useState (parseInt(localStorage.getItem("localCookiesPerSecond"))||1);
+    const [Grandma, setGrandma] =useState (parseInt(localStorage.getItem("cookieCountGrandma"))||0);
+    console.log(cookies, CookiesPerSecond, Grandma)
+    const [Oven, setOven] =useState (parseInt(localStorage.getItem("cookieCountOven"))||0);
+    const [Factory, setFactory] =useState (parseInt(localStorage.getItem("cookieCountFactory"))||0);
+    const [Mine, setMine] =useState (parseInt(localStorage.getItem("cookieCountMine"))||0);
+    const [Bank, setBank] =useState (parseInt(localStorage.getItem("cookieCountBank"))||0);
     useEffect(()=>{
       const myCookies = setInterval(()=>{
+        console.log(CookiesPerSecond, cookies)
         setCookies((currentCookies)=>currentCookies + CookiesPerSecond);
       },1000);
       return()=>{
         clearInterval(myCookies);
       };
     },[]);
+    useEffect(()=>{
+      localStorage.setItem("cookieCountGrandma",Grandma );
+      localStorage.setItem("cookieCount", cookies);
+      localStorage.setItem("localCookiesPerSecond", CookiesPerSecond);
+      localStorage.setItem("cookieCountOven",Oven );
+      localStorage.setItem("cookieCountFactory",Factory );
+      localStorage.setItem("cookieCountMine",Mine );
+      localStorage.setItem("cookieCountBank",Bank );
+    },[Grandma,cookies,CookiesPerSecond]);
     function handleClick(){
       setCookies(cookies + CookiesPerSecond);
+
     }
+    function addGrandma(){
+      if(cookies>=10){
+      setGrandma(Grandma + 1) ;
+      setCookiesPerSecond(CookiesPerSecond+1);
+      setCookies(cookies-10);
+            }else{
+          alert("you dont have enought cookies")
+      }
+    }
+      function addOven(){
+        if(cookies>=100){
+        setOven(Oven + 1) ;
+        setCookiesPerSecond(CookiesPerSecond+10);
+        setCookies(cookies-100);
+               }else{
+            alert("you dont have enought cookies")
+        }}
+        function addFactory(){
+          if(cookies>=1000){
+          setFactory(Factory + 1) ;
+          setCookiesPerSecond(CookiesPerSecond+100);
+          setCookies(cookies-1000);
+        
+          }else{
+              alert("you dont have enought cookies")
+          }}
+          function addMine(){
+            if(cookies>=10000){
+            setMine(Mine + 1) ;
+            setCookiesPerSecond(CookiesPerSecond+1000);
+            setCookies(cookies-10000);
+        
+            }else{
+                alert("you dont have enought cookies")
+            }}
+            function addBank(){
+              if(cookies>=100000){
+              setBank(Bank + 1) ;
+              setCookiesPerSecond(CookiesPerSecond+10000);
+              setCookies(cookies-100000);
+           
+              }else{
+                  alert("you dont have enought cookies")
+              }
+  }
   return(
     
    
@@ -23,7 +86,9 @@ export default function  App(){
          <p id="counterPS" class="pcounterPS">{CookiesPerSecond} CPS</p>
         <button onClick = {handleClick} id="counterbutton"></button>
       </div>
-      <ShopComponent/>
+      <ShopComponent cookies={cookies} CookiesPerSecond={CookiesPerSecond} 
+      Grandma={Grandma} Oven={Oven} Factory={Factory} Mine={Mine} Bank={Bank}
+      addGrandma={addGrandma} addOven={addOven} addFactory={addFactory} addMine={addMine} addBank={addBank} />
     </div>
    );
 }
--- src/ShopComponent.jsx
+++ src/ShopComponent.jsx
@@ -1,42 +1,44 @@
 import cm from "./images/CM.png";
-export default function ShopComponent(){
+export default function ShopComponent(props){
+    const Button = ({onclickHandler, id, max, total})=>{
+        const background = total>=max ? "green":"red";
+               return (
+            <button style={{backgroundColor:background}}onClick = {onclickHandler} id={id} >Buy</button>
+        )
+    }
    return(
             <div id="divform">
-            <form>
-                <button id="counterGrandma">0</button>
+            
+                <button id="counterGrandma">{props.Grandma}</button>
                 <input type="text" value="Grandma"/>
                 <input  type="text" value="10 C"/>
                 <input  type="text" value="+1"/>
-                <button id="buyGrandma">Buy</button><br/><br/>
+                <Button onclickHandler={props.addGrandma} id="buyGrandma" max={10} total ={props.cookies}/><br/><br/>
 
-
-                <button id="counterOven">0</button>
+                <button id="counterOven">{props.Oven}</button>
                 <input type="text" value="Oven"/>
                 <input  type="text" value="100 C"/>
                 <input  type="text" value="+10"/>
-                <button id="buyOven">Buy</button><br/><br/>
+               <Button onclickHandler={props.addOven} id="buyOven" max={100} total ={props.cookies}/><br/><br/>
 
-
-                <button id="counterFactory">0</button>
+                <button id="counterFactory">{props.Factory}</button>
                 <input type="text" value="Factory"/>
                 <input  type="text" value="1000 C"/>
                 <input  type="text" value="+100"/>
-                <button id="buyFactory">Buy</button><br/><br/>
+                <Button onclickHandler={props.addFactory} id="buyFactory" max={1000} total ={props.cookies}/><br/><br/>
 
-
-                <button id="counterMine">0</button>
+                <button id="counterMine">{props.Mine}</button>
                 <input type="text" value="Mine"/>
                 <input  type="text" value="10000 C"/>
                 <input  type="text" value="+1000"/>
-                <button id="buyMine">Buy</button><br/><br/>
+                <Button onclickHandler={props.addMine} id="buyMine" max={10000} total ={props.cookies}/><br/><br/>
 
-
-                <button id="counterBank">0</button>
+                <button id="counterBank">{props.Bank}</button>
                 <input type="text" value="Bank"/>
                 <input  type="text" value="1000000 C"/>
                 <input  type="text" value="+10000"/>
-                <button id="buyBank">Buy</button><br/><br/>
-            </form>
+                <Button onclickHandler={props.addBank} id="buyBank" max={100000} total ={props.cookies}/><br/><br/>
+
             <img id="resetid" src={cm}/>
         </div>
 ) ;
